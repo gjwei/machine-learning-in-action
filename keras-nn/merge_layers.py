b1607e4d19530c10d705f1e4171c25b48cb4676f
1449894353 <1449894353@qq.com>
2017-09-24 11:07:53 +0800

    对titanic数据使用了svm进行分类

---
+++ keras-nn/merge_layers.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+ created by gjwei on 9/23/17
+  
+"""
+from keras.layers import (Embedding, Dense, Input,
+                          concatenate, LSTM
+                          )
+from keras.models import Model
+
+
+main_input = Input(shape=(100, ), dtype='int32', name='main_input')
+
+x = Embedding(output_dim=500, input_dim=10000, input_length=100)(main_input)
+
+lstm_out = LSTM(32)(x)
+
+auxiliary_output = Dense(1, activation='sigmoid', name='aux_output')(lstm_out)
+
+auxiliary_input = Input(shape=(5,), name='aux_input')
+
+x = concatenate(inputs=[main_input, auxiliary_input])
+
+# We stack a deep densely-connected network on top
+x = Dense(64, activation='relu')(x)
+x = Dense(64, activation='relu')(x)
+x = Dense(64, activation='relu')(x)
+
+# And finally we add the main logistic regression layer
+main_output = Dense(1, activation='sigmoid', name='main_output')(x)
+
+model = Model(inputs=[main_input, auxiliary_input], outputs=[main_output, auxiliary_output])
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy',
+              loss_weights=[1., 0.2])
+
+model.compile(optimizer='rmsprop',
+              loss={'main_output': 'binary_crossentropy', 'aux_output': 'binary_crossentropy'},
+              loss_weights={'main_output': 1., 'aux_output': 0.2})
+
+# And trained it via:
+model.fit({'main_input': headline_data, 'aux_input': additional_data},
+          {'main_output': labels, 'aux_output': labels},
+          epochs=50, batch_size=32)
+
---
+++ titanic/read_data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" 
+ created by gjwei on 9/23/17
+  
+"""
+
+
